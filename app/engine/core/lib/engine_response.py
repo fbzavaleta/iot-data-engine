c81fbef21771fc445884bf0dd4567385a76b26c0
fbzavaleta <benjamin.zavaleta@crunchyroll.com>
2024-03-21 23:49:25 +0000

    adding lib module to project

---
+++ app/engine/core/lib/engine_response.py
@@ -0,0 +1,44 @@
+from functools import lru_cache as memoized
+from typing import Dict, Union
+import ast
+from flask import Request
+
+from engine.core.enums.engine import EngineEndpoint
+"""
+Author: Francis Benjamin Zavaleta, Eng
+Copyright © fbzavaleta. All rights reserved.
+"""
+
+class EngineResponse:
+    def __init__(self, request:Request) -> None:
+        self.request = request
+
+    @property
+    @memoized(maxsize=1)
+    def fetch_query_parameters(self) -> Union[Dict, None]:
+        if not self.get_query_parameters:
+            return None
+        return self.get_query_parameters.to_dict()
+    
+    @property
+    @memoized(maxsize=1)
+    def fetch_body_data(self) -> Union[Dict, None]:
+        try:
+            return self.bytes2dict(self.get_body_data)
+        except:
+            return None
+
+    @property
+    @memoized(maxsize=1)
+    def get_query_parameters(self) -> dict:
+        return self.request.args
+
+    @property
+    @memoized(maxsize=1)
+    def get_body_data(self) -> bytes:
+        return self.request.get_data()
+
+    @staticmethod
+    def bytes2dict(data: bytes) -> Dict:
+        decoded_data = data.decode('UTF-8')
+        return ast.literal_eval(decoded_data)
---
+++ app/engine/core/lib/thingspeaks_response.py
@@ -0,0 +1,52 @@
+import requests
+from functools import lru_cache as memoized
+from typing import Dict, List, Union, Iterator
+
+from engine.core.enums.thingspeaks import (
+    ApiConstants, ApiParameters, ApiChannelResponse, ApiFeedResponse
+) 
+"""
+Author: Francis Benjamin Zavaleta, Eng
+Copyright © fbzavaleta. All rights reserved.
+"""
+
+class ThingSpeaksRequestResponse:
+    def __init__(self, channel_id: str, api_key: str=None, n_rows: int=None
+                 , interval: int=30) -> None:
+        self.parameters = ApiParameters(api_key=api_key, n_rows=n_rows, channel_id=channel_id
+                                        , interval=interval)
+        self._constants = ApiConstants
+
+    @property
+    @memoized(maxsize=1)
+    def get_channel_response(self)-> Union[ApiChannelResponse, None]:
+        if not self._get_thinkspeaks_response:
+            return None
+        response_from_channel = self._get_thinkspeaks_response.get(self._constants.channel_key)
+        if not response_from_channel:
+            return None
+        return ApiChannelResponse(**response_from_channel)
+
+    @property
+    @memoized(maxsize=1)
+    def get_feed_response(self)-> Union[Iterator[Dict], None]:
+        if not self._get_thinkspeaks_response:
+            return None
+        response_from_feed = self._get_thinkspeaks_response.get(self._constants.feed_key)
+        for feed in response_from_feed:
+            yield feed
+
+    @property
+    @memoized(maxsize=1)
+    def _get_url(self) -> str:
+        return f"{self._constants.url_base}/channels/{self.parameters.channel_id}/feeds.json?api_key=\
+                {self.parameters.api_key}&results={self.parameters.n_rows}"
+    
+    @property
+    @memoized(maxsize=1)
+    def _get_thinkspeaks_response(self) -> Union[Dict, None]:
+        json_response = None
+        response =  requests.get(self._get_url)
+        if response.status_code == 200:
+            json_response = response.json()
+        return json_response
